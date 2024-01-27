09058851c22e7ab2c51686a952aa768c39731cfd
30N6 <55774026+30N6@users.noreply.github.com>
2024-01-27 15:33:23 -0700

    Work in progress: dwell control.

---
+++ esm_receiver/hdl/esm_config.vhd
@@ -0,0 +1,168 @@
+library ieee;
+  use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
+
+library common_lib;
+  use common_lib.common_pkg.all;
+
+library esm_lib;
+  use esm_lib.esm_pkg.all;
+
+entity esm_config is
+generic (
+  AXI_DATA_WIDTH : natural
+);
+port (
+  Clk           : in  std_logic;
+  Rst           : in  std_logic;
+
+  Axis_ready    : out std_logic;
+  Axis_valid    : in  std_logic;
+  Axis_last     : in  std_logic;
+  Axis_data     : in  std_logic_vector(AXI_DATA_WIDTH - 1 downto 0);
+
+  Rst_out       : out std_logic;
+  Enable_chan   : out std_logic_vector(1 downto 0);
+  Enable_pdw    : out std_logic_vector(1 downto 0);
+
+  Module_config : out esm_config_data_t
+);
+end entity esm_config;
+
+architecture rtl of esm_config is
+
+  type state_t is
+  (
+    S_WORD_0,
+    S_WORD_1,
+    S_WORD_2,
+    S_DROP,
+    S_ACTIVE_CONFIG_CONTROL,
+    S_ACTIVE_CONFIG_MODULE
+  );
+
+  signal r_axis_valid     : std_logic;
+  signal r_axis_last      : std_logic;
+  signal r_axis_data      : std_logic_vector(AXI_DATA_WIDTH - 1 downto 0);
+
+  signal s_state          : state_t;
+
+  signal w_module_id      : unsigned(ESM_MODULE_ID_WIDTH - 1 downto 0);
+  signal w_message_type   : unsigned(ESM_MESSAGE_TYPE_WIDTH - 1 downto 0);
+  signal r_module_id      : unsigned(ESM_MODULE_ID_WIDTH - 1 downto 0);
+  signal r_message_type   : unsigned(ESM_MESSAGE_TYPE_WIDTH - 1 downto 0);
+  signal r_first          : std_logic;
+
+  signal r_module_config  : esm_config_data_t;
+
+begin
+
+  assert (AXI_DATA_WIDTH = 32)
+    report "AXI data width expected to be 32."
+    severity failure;
+
+  Axis_ready <= '1';
+
+  process(Clk)
+  begin
+    if rising_edge(Clk) then
+      r_axis_valid  <= Axis_valid;
+      r_axis_last   <= Axis_last;
+      r_axis_data   <= Axis_data;
+    end if;
+  end process;
+
+
+  w_module_id     <= unsigned(r_axis_data(24 + ESM_MODULE_ID_WIDTH - 1 downto 24));
+  w_message_type  <= unsigned(r_axis_data(16 + ESM_MESSAGE_TYPE_WIDTH - 1 downto 16));
+
+  process(Clk)
+  begin
+    if rising_edge(Clk) then
+      if (Rst = '1') then
+        s_state <= WORD_0;
+      else
+        if (r_axis_valid = '1') then
+          case s_state is
+          when S_WORD_0 =>
+            if (r_axis_data = ESM_CONTROL_MAGIC_NUM) then
+              s_state <= S_WORD_1;
+            else
+              s_state <= S_DROP;
+            end if;
+
+          when S_WORD_1 =>
+            s_state <= S_WORD_2;
+
+          when S_WORD_2 =>
+            if (w_module_id = ESM_MODULE_ID_CONTROL) then
+              s_state <= S_ACTIVE_CONFIG_CONTROL;
+            else
+              s_state <= S_ACTIVE_CONFIG_MODULE;
+            end if;
+
+          when S_DROP =>
+            s_state <= S_DROP;
+
+          when S_ACTIVE_CONFIG_CONTROL =>
+            s_state <= S_ACTIVE_CONFIG_CONTROL;
+
+          when S_ACTIVE_CONFIG_MODULE =>
+            s_state <= S_ACTIVE_CONFIG_MODULE;
+
+          end case;
+
+          if (r_axis_last = '1') then
+            s_state <= WORD_0;
+          end if;
+        end if;
+      end if;
+    end if;
+  end process;
+
+  process(Clk)
+  begin
+    if rising_edge(Clk) then
+      if (s_state = S_WORD_2) then
+        r_module_id     <= w_module_id;
+        r_message_type  <= w_message_type;
+      end if;
+
+      if (r_axis_valid = '1') then
+        r_first <= to_stdlogic(s_state = S_WORD_2);
+      end if;
+    end if;
+  end process;
+
+  process(Clk)
+  begin
+    if rising_edge(Clk) then
+      r_module_config.valid         <= r_axis_valid and to_stdlogic(s_state = S_ACTIVE_CONFIG_MODULE);
+      r_module_config.first         <= r_first;
+      r_module_config.last          <= r_axis_last;
+      r_module_config.data          <= r_axis_data;
+      r_module_config.module_id     <= r_module_id;
+      r_module_config.message_type  <= r_message_type;
+    end if;
+  end process;
+
+  Module_config <= r_module_config;
+
+  process(Clk)
+  begin
+    if rising_edge(Clk) then
+      if (Rst = '1') then
+        Rst_out     <= '0';
+        Enable_chan <= (others => '0');
+        Enable_pdw  <= (others => '0');
+      else
+        if ((r_axis_valid = '1') and (s_state = S_ACTIVE_CONFIG_CONTROL) and (r_first = '1') and (r_message_type = ESM_CONTROL_MESSAGE_TYPE_ENABLE)) then
+          Rst_out     <= r_axis_data(24);
+          Enable_chan <= r_axis_data(17 downto 16);
+          Enable_pdw  <= r_axis_data(9 downto 8);
+        end if;
+      end if;
+    end if;
+  end process;
+
+end architecture rtl;
--- esm_receiver/hdl/esm_pkg.vhd
+++ esm_receiver/hdl/esm_pkg.vhd
@@ -10,90 +10,127 @@ package esm_pkg is
   constant ESM_CONTROL_MAGIC_NUM                        : std_logic_vector(31 downto 0) := "45534D43";
   constant ESM_REPORT_MAGIC_NUM                         : std_logic_vector(31 downto 0) := "45534D52";
 
-  constant ESM_CONTROL_MESSAGE_TYPE_ENABLE              : unsigned(7 downto 0) := x"00";
-  constant ESM_CONTROL_MESSAGE_TYPE_DWELL_REQUEST       : unsigned(7 downto 0) := x"01";
-  constant ESM_CONTROL_MESSAGE_TYPE_PDW_SETUP           : unsigned(7 downto 0) := x"02";  --TODO: unused?
+  constant ESM_MODULE_ID_WIDTH                          : natural := 2;
+  constant ESM_MESSAGE_TYPE_WIDTH                       : natural := 8;
 
-  constant ESM_REPORT_MESSAGE_TYPE_DWELL_COMPLETE_INFO  : unsigned(7 downto 0) := x"10";
-  constant ESM_REPORT_MESSAGE_TYPE_DWELL_COMPLETE_STATS : unsigned(7 downto 0) := x"10";
-  constant ESM_REPORT_MESSAGE_TYPE_PDW                  : unsigned(7 downto 0) := x"20";
+  constant ESM_MODULE_ID_CONTROL                        : unsigned(ESM_MODULE_ID_WIDTH - 1 downto 0) := "00";
+  constant ESM_MODULE_ID_DWELL                          : unsigned(ESM_MODULE_ID_WIDTH - 1 downto 0) := "01";
+  constant ESM_MODULE_ID_PDW                            : unsigned(ESM_MODULE_ID_WIDTH - 1 downto 0) := "10";
 
-  constant ESM_MAX_NUM_CHANNELS                   : natural := 64;
-  constant ESM_NUM_FAST_LOCK_PROFILES             : natural := 8;
+  constant ESM_CONTROL_MESSAGE_TYPE_ENABLE              : unsigned(ESM_MESSAGE_TYPE_WIDTH - 1 downto 0) := x"00";
+  constant ESM_CONTROL_MESSAGE_TYPE_DWELL_ENTRY         : unsigned(ESM_MESSAGE_TYPE_WIDTH - 1 downto 0) := x"01";
+  constant ESM_CONTROL_MESSAGE_TYPE_DWELL_PROGRAM       : unsigned(ESM_MESSAGE_TYPE_WIDTH - 1 downto 0) := x"02";
+  constant ESM_CONTROL_MESSAGE_TYPE_PDW_SETUP           : unsigned(ESM_MESSAGE_TYPE_WIDTH - 1 downto 0) := x"03";  --TODO: unused?
+
+  constant ESM_REPORT_MESSAGE_TYPE_DWELL_COMPLETE_INFO  : unsigned(ESM_MESSAGE_TYPE_WIDTH - 1 downto 0) := x"10";
+  constant ESM_REPORT_MESSAGE_TYPE_DWELL_COMPLETE_STATS : unsigned(ESM_MESSAGE_TYPE_WIDTH - 1 downto 0) := x"10";
+  constant ESM_REPORT_MESSAGE_TYPE_PDW                  : unsigned(ESM_MESSAGE_TYPE_WIDTH - 1 downto 0) := x"20";
+
+  constant ESM_NUM_CHANNELS_NARROW                      : natural := 64;
+  constant ESM_NUM_CHANNELS_WIDE                        : natural := 8;
+  constant ESM_CHANNEL_INDEX_WIDTH                      : natural := clog2(ESM_NUM_CHANNELS_WIDE);
+
+  constant ESM_NUM_FAST_LOCK_PROFILES                   : natural := 8;
+  constant ESM_FAST_LOCK_PROFILE_INDEX_WIDTH            : natural := clog2(ESM_NUM_FAST_LOCK_PROFILES);
+  constant ESM_NUM_DWELL_ENTRIES                        : natural := 32;
+  constant ESM_DWELL_ENTRY_INDEX_WIDTH                  : natural := clog2(ESM_NUM_DWELL_ENTRIES);
+  constant ESM_NUM_DWELL_INSTRUCTIONS                   : natural := 32;
+  constant ESM_DWELL_INSTRUCTION_INDEX_WIDTH            : natural := clog2(ESM_NUM_DWELL_INSTRUCTIONS);
 
   type esm_common_header_t is record
-    magic_num     : std_logic_vector(31 downto 0);
-    message_type  : unsigned(7 downto 0);
-    module_id     : unsigned(7 downto 0);
-    padding_0     : std_logic_vector(15 downto 0);
-    sequence_num  : unsigned(31 downto 0);
-    padding_1     : std_logic_vector(31 downto 0);
+    magic_num                 : std_logic_vector(31 downto 0);
+    sequence_num              : unsigned(31 downto 0);
+    module_id                 : unsigned(ESM_MODULE_ID_WIDTH - 1 downto 0);
+    message_type              : unsigned(ESM_MESSAGE_TYPE_WIDTH - 1 downto 0);
   end record;
 
   type esm_message_enable_t is record
     header                    : esm_common_header_t;
     reset                     : std_logic;
-    enable_channelizer_wide   : std_logic;
-    enable_channelizer_narrow : std_logic;
-    enable_pdw_wide           : std_logic;
-    enable_pdw_narrow         : std_logic;
+    enable_channelizer        : std_logic_vector(1 downto 0);
+    enable_pdw                : std_logic_vector(1 downto 0);
   end record;
 
   type esm_dwell_metadata_t is record
-    tag           : unsigned(31 downto 0);
-    frequency     : unsigned(15 downto 0);
-    gain          : unsigned(7 downto 0);
-    padding       : unsigned(7 downto 0);
+    tag                       : unsigned(15 downto 0);
+    frequency                 : unsigned(15 downto 0);
+    duration                  : unsigned(31 downto 0);
+    gain                      : unsigned(7 downto 0);
+    fast_lock_profile         : unsigned(ESM_FAST_LOCK_PROFILE_INDEX_WIDTH - 1 downto 0);
+    threshold_narrow          : unsigned(15 downto 0);
+    threshold_wide            : unsigned(15 downto 0);
+    channel_mask_narrow       : std_logic_vector(ESM_NUM_CHANNELS_NARROW - 1 downto 0);
+    channel_mask_wide         : std_logic_vector(ESM_NUM_CHANNELS_WIDE - 1 downto 0);
   end record;
 
-  type esm_dwell_request_entry_t is record
-    valid         : std_logic;
-    metadata      : esm_dwell_metadata_t;
-    threshold     : unsigned(31 downto 0);
-    duration      : unsigned(31 downto 0);
-    channel_mask  : std_logic_vector(ESM_MAX_NUM_CHANNELS - 1 downto 0);
+  type esm_message_dwell_entry_t is record
+    header                    : esm_common_header_t;
+    entry_valid               : std_logic;
+    entry_data                : esm_dwell_metadata_t;
+    entry_index               : unsigned(ESM_DWELL_ENTRY_INDEX_WIDTH - 1 downto 0);
   end record;
 
-  type esm_dwell_request_entry_array_t is array (natural range <>) of esm_dwell_request_entry_t;
+  type esm_dwell_instruction_t is record
+    valid                     : std_logic;
+    global_counter_check      : std_logic;
+    global_counter_dec        : std_logic;
+    repeat_count              : unsigned(3 downto 0);
+    entry_index               : unsigned(ESM_DWELL_ENTRY_INDEX_WIDTH - 1 downto 0);
+    next_instruction_index    : unsigned(ESM_DWELL_INSTRUCTION_INDEX_WIDTH - 1 downto 0);
+  end record;
 
-  type esm_message_dwell_request_t is record
-    header        : esm_common_header_t;
-    dwell_entries : esm_dwell_request_entry_array_t(ESM_NUM_FAST_LOCK_PROFILES - 1 downto 0);
+  type esm_dwell_instruction_array_t is array (natural range <>) of esm_dwell_instruction_t;
+
+  type esm_message_dwell_program_t is record
+    header                    : esm_common_header_t;
+    enable_program            : std_logic;
+    enable_delayed_start      : std_logic;
+    global_counter_init       : unsigned(31 downto 0);
+    delayed_start_time        : unsigned(63 downto 0);
+    instructions              : esm_dwell_instruction_array_t(ESM_NUM_DWELL_INSTRUCTIONS - 1 downto 0);
   end record;
 
   type esm_message_dwell_complete_info_t is record
-    header              : esm_common_header_t;
-    dwell_sequence_num  : unsigned(31 downto 0);
-    metadata            : esm_dwell_metadata_t;
+    header                    : esm_common_header_t;
+    dwell_sequence_num        : unsigned(31 downto 0);
+    metadata                  : esm_dwell_metadata_t;
 
-    channel_mask        : std_logic_vector(ESM_MAX_NUM_CHANNELS - 1 downto 0);
-    num_samples         : unsigned(31 downto 0);
-    ts_start_dwell      : unsigned(63 downto 0);
-    ts_pll_locked       : unsigned(63 downto 0);
-    ts_dwell_end        : unsigned(63 downto 0);
+    num_samples               : unsigned(31 downto 0);
+    ts_dwell_start            : unsigned(63 downto 0);
+    ts_pll_locked             : unsigned(63 downto 0);
+    ts_dwell_end              : unsigned(63 downto 0);
   end record;
 
   type esm_message_dwell_complete_stats_t is record
-    header              : esm_common_header_t;
-    dwell_sequence_num  : unsigned(31 downto 0);
-    metadata            : esm_dwell_metadata_t;
+    header                    : esm_common_header_t;
+    dwell_sequence_num        : unsigned(31 downto 0);
+    metadata                  : esm_dwell_metadata_t;
 
-    channel_amplitude   : unsigned_array_t(ESM_MAX_NUM_CHANNELS/2 - 1 downto 0)(31 downto 0);
-    channel_start_index : unsigned(7 downto 0);
+    channel_start_index       : unsigned(ESM_CHANNEL_INDEX_WIDTH - 1 downto 0);
+    channel_amplitude         : unsigned_array_t(ESM_NUM_CHANNELS_WIDE/2 - 1 downto 0)(31 downto 0);
   end record;
 
   type esm_message_pdw_t is record
-    header              : esm_common_header_t;
-    pdw_sequence_num    : unsigned(31 downto 0);
-    dwell_sequence_num  : unsigned(31 downto 0);
-    dwell_metadata      : esm_dwell_metadata_t;
-    threshold           : unsigned(31 downto 0);
-    pulse_amplitude     : unsigned(31 downto 0);
-    pulse_duration      : unsigned(31 downto 0);  --TODO: early termination flag?
-    pulse_frequency     : unsigned(31 downto 0);
-    ts_start            : unsigned(63 downto 0);
+    header                    : esm_common_header_t;
+    pdw_sequence_num          : unsigned(31 downto 0);
+    dwell_sequence_num        : unsigned(31 downto 0);
+    dwell_metadata            : esm_dwell_metadata_t;
+    pulse_channel             : unsigned(ESM_CHANNEL_INDEX_WIDTH - 1 downto 0);
+    pulse_threshold           : unsigned(31 downto 0);
+    pulse_amplitude           : unsigned(31 downto 0);
+    pulse_duration            : unsigned(31 downto 0);  --TODO: early termination flag?
+    pulse_frequency           : unsigned(31 downto 0);  --TODO: IFM module
+    pulse_start_time          : unsigned(63 downto 0);
+    raw_samples               : std_logic_vector_array_t(40 downto 0)(31 downto 0); --TODO: increase to max
+  end record;
 
-    raw_samples         : std_logic_vector_array_t(49 downto 0)(31 downto 0);
+  type esm_config_data_t is record
+    valid                     : std_logic;
+    first                     : std_logic;
+    last                      : std_logic;
+    data                      : std_logic_vector(31 downto 0);
+    module_id                 : unsigned(ESM_MODULE_ID_WIDTH - 1 downto 0);
+    message_type              : unsigned(ESM_MESSAGE_TYPE_WIDTH - 1 downto 0);
   end record;
 
 end package esm_pkg;
