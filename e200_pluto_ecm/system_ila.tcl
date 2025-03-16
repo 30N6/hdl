7c86984dec6c66af661e67de9f329969e947951e
30N6 <55774026+30N6@users.noreply.github.com>
2025-03-16 17:17:41 -0600

    ILA script

--- e200_pluto_ecm/system_ila.tcl
+++ e200_pluto_ecm/system_ila.tcl
@@ -1,5 +1,5 @@
 create_debug_core u_ila_0 ila
-set_property C_DATA_DEPTH 4096 [get_debug_cores u_ila_0]
+set_property C_DATA_DEPTH 1024 [get_debug_cores u_ila_0]
 set_property C_TRIGIN_EN false [get_debug_cores u_ila_0]
 set_property C_TRIGOUT_EN false [get_debug_cores u_ila_0]
 set_property C_ADV_TRIGGER false [get_debug_cores u_ila_0]
@@ -8,21 +8,21 @@ set_property C_EN_STRG_QUAL false [get_debug_cores u_ila_0]
 set_property ALL_PROBE_SAME_MU true [get_debug_cores u_ila_0]
 set_property ALL_PROBE_SAME_MU_CNT 1 [get_debug_cores u_ila_0]
 connect_debug_port u_ila_0/clk [get_nets [list i_system_wrapper/system_i/axi_ad9361/inst/i_dev_if/i_clk/clk ]]
-set_property port_width 16 [get_debug_ports u_ila_0/probe0]
+set_property port_width 8 [get_debug_ports u_ila_0/probe0]
 set_property PROBE_TYPE DATA_AND_TRIGGER [get_debug_ports u_ila_0/probe0]
-connect_debug_port u_ila_0/probe0 [get_nets [list {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_i[0]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_i[1]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_i[2]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_i[3]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_i[4]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_i[5]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_i[6]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_i[7]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_i[8]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_i[9]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_i[10]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_i[11]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_i[12]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_i[13]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_i[14]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_i[15]} ]]
+connect_debug_port u_ila_0/probe0 [get_nets [list {i_system_wrapper/system_i/ecm_top/U0/w_Ad9361_status[0]} {i_system_wrapper/system_i/ecm_top/U0/w_Ad9361_status[1]} {i_system_wrapper/system_i/ecm_top/U0/w_Ad9361_status[2]} {i_system_wrapper/system_i/ecm_top/U0/w_Ad9361_status[3]} {i_system_wrapper/system_i/ecm_top/U0/w_Ad9361_status[4]} {i_system_wrapper/system_i/ecm_top/U0/w_Ad9361_status[5]} {i_system_wrapper/system_i/ecm_top/U0/w_Ad9361_status[6]} {i_system_wrapper/system_i/ecm_top/U0/w_Ad9361_status[7]} ]]
 create_debug_port u_ila_0 probe
-set_property port_width 8 [get_debug_ports u_ila_0/probe1]
+set_property port_width 16 [get_debug_ports u_ila_0/probe1]
 set_property PROBE_TYPE DATA_AND_TRIGGER [get_debug_ports u_ila_0/probe1]
-connect_debug_port u_ila_0/probe1 [get_nets [list {i_system_wrapper/system_i/ecm_top/U0/w_Ad9361_status[0]} {i_system_wrapper/system_i/ecm_top/U0/w_Ad9361_status[1]} {i_system_wrapper/system_i/ecm_top/U0/w_Ad9361_status[2]} {i_system_wrapper/system_i/ecm_top/U0/w_Ad9361_status[3]} {i_system_wrapper/system_i/ecm_top/U0/w_Ad9361_status[4]} {i_system_wrapper/system_i/ecm_top/U0/w_Ad9361_status[5]} {i_system_wrapper/system_i/ecm_top/U0/w_Ad9361_status[6]} {i_system_wrapper/system_i/ecm_top/U0/w_Ad9361_status[7]} ]]
+connect_debug_port u_ila_0/probe1 [get_nets [list {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_q[0]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_q[1]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_q[2]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_q[3]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_q[4]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_q[5]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_q[6]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_q[7]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_q[8]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_q[9]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_q[10]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_q[11]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_q[12]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_q[13]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_q[14]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_q[15]} ]]
 create_debug_port u_ila_0 probe
 set_property port_width 16 [get_debug_ports u_ila_0/probe2]
 set_property PROBE_TYPE DATA_AND_TRIGGER [get_debug_ports u_ila_0/probe2]
-connect_debug_port u_ila_0/probe2 [get_nets [list {i_system_wrapper/system_i/ecm_top/U0/w_Adc_data_i[0]} {i_system_wrapper/system_i/ecm_top/U0/w_Adc_data_i[1]} {i_system_wrapper/system_i/ecm_top/U0/w_Adc_data_i[2]} {i_system_wrapper/system_i/ecm_top/U0/w_Adc_data_i[3]} {i_system_wrapper/system_i/ecm_top/U0/w_Adc_data_i[4]} {i_system_wrapper/system_i/ecm_top/U0/w_Adc_data_i[5]} {i_system_wrapper/system_i/ecm_top/U0/w_Adc_data_i[6]} {i_system_wrapper/system_i/ecm_top/U0/w_Adc_data_i[7]} {i_system_wrapper/system_i/ecm_top/U0/w_Adc_data_i[8]} {i_system_wrapper/system_i/ecm_top/U0/w_Adc_data_i[9]} {i_system_wrapper/system_i/ecm_top/U0/w_Adc_data_i[10]} {i_system_wrapper/system_i/ecm_top/U0/w_Adc_data_i[11]} {i_system_wrapper/system_i/ecm_top/U0/w_Adc_data_i[12]} {i_system_wrapper/system_i/ecm_top/U0/w_Adc_data_i[13]} {i_system_wrapper/system_i/ecm_top/U0/w_Adc_data_i[14]} {i_system_wrapper/system_i/ecm_top/U0/w_Adc_data_i[15]} ]]
+connect_debug_port u_ila_0/probe2 [get_nets [list {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_i[0]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_i[1]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_i[2]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_i[3]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_i[4]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_i[5]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_i[6]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_i[7]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_i[8]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_i[9]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_i[10]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_i[11]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_i[12]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_i[13]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_i[14]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_i[15]} ]]
 create_debug_port u_ila_0 probe
 set_property port_width 16 [get_debug_ports u_ila_0/probe3]
 set_property PROBE_TYPE DATA_AND_TRIGGER [get_debug_ports u_ila_0/probe3]
-connect_debug_port u_ila_0/probe3 [get_nets [list {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_q[0]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_q[1]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_q[2]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_q[3]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_q[4]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_q[5]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_q[6]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_q[7]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_q[8]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_q[9]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_q[10]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_q[11]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_q[12]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_q[13]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_q[14]} {i_system_wrapper/system_i/ecm_top/U0/w_Dac_data_q[15]} ]]
+connect_debug_port u_ila_0/probe3 [get_nets [list {i_system_wrapper/system_i/ecm_top/U0/w_Adc_data_i[0]} {i_system_wrapper/system_i/ecm_top/U0/w_Adc_data_i[1]} {i_system_wrapper/system_i/ecm_top/U0/w_Adc_data_i[2]} {i_system_wrapper/system_i/ecm_top/U0/w_Adc_data_i[3]} {i_system_wrapper/system_i/ecm_top/U0/w_Adc_data_i[4]} {i_system_wrapper/system_i/ecm_top/U0/w_Adc_data_i[5]} {i_system_wrapper/system_i/ecm_top/U0/w_Adc_data_i[6]} {i_system_wrapper/system_i/ecm_top/U0/w_Adc_data_i[7]} {i_system_wrapper/system_i/ecm_top/U0/w_Adc_data_i[8]} {i_system_wrapper/system_i/ecm_top/U0/w_Adc_data_i[9]} {i_system_wrapper/system_i/ecm_top/U0/w_Adc_data_i[10]} {i_system_wrapper/system_i/ecm_top/U0/w_Adc_data_i[11]} {i_system_wrapper/system_i/ecm_top/U0/w_Adc_data_i[12]} {i_system_wrapper/system_i/ecm_top/U0/w_Adc_data_i[13]} {i_system_wrapper/system_i/ecm_top/U0/w_Adc_data_i[14]} {i_system_wrapper/system_i/ecm_top/U0/w_Adc_data_i[15]} ]]
 create_debug_port u_ila_0 probe
 set_property port_width 16 [get_debug_ports u_ila_0/probe4]
 set_property PROBE_TYPE DATA_AND_TRIGGER [get_debug_ports u_ila_0/probe4]
@@ -32,9 +32,29 @@ set_property port_width 4 [get_debug_ports u_ila_0/probe5]
 set_property PROBE_TYPE DATA_AND_TRIGGER [get_debug_ports u_ila_0/probe5]
 connect_debug_port u_ila_0/probe5 [get_nets [list {i_system_wrapper/system_i/ecm_top/U0/w_Ad9361_control_o[0]} {i_system_wrapper/system_i/ecm_top/U0/w_Ad9361_control_o[1]} {i_system_wrapper/system_i/ecm_top/U0/w_Ad9361_control_o[2]} {i_system_wrapper/system_i/ecm_top/U0/w_Ad9361_control_o[3]} ]]
 create_debug_port u_ila_0 probe
-set_property port_width 1 [get_debug_ports u_ila_0/probe6]
+set_property port_width 24 [get_debug_ports u_ila_0/probe6]
 set_property PROBE_TYPE DATA_AND_TRIGGER [get_debug_ports u_ila_0/probe6]
-connect_debug_port u_ila_0/probe6 [get_nets [list i_system_wrapper/system_i/ecm_top/U0/w_Adc_valid ]]
+connect_debug_port u_ila_0/probe6 [get_nets [list {i_system_wrapper/system_i/axi_ad9361/inst/dac_data_s[0]} {i_system_wrapper/system_i/axi_ad9361/inst/dac_data_s[1]} {i_system_wrapper/system_i/axi_ad9361/inst/dac_data_s[2]} {i_system_wrapper/system_i/axi_ad9361/inst/dac_data_s[3]} {i_system_wrapper/system_i/axi_ad9361/inst/dac_data_s[4]} {i_system_wrapper/system_i/axi_ad9361/inst/dac_data_s[5]} {i_system_wrapper/system_i/axi_ad9361/inst/dac_data_s[6]} {i_system_wrapper/system_i/axi_ad9361/inst/dac_data_s[7]} {i_system_wrapper/system_i/axi_ad9361/inst/dac_data_s[8]} {i_system_wrapper/system_i/axi_ad9361/inst/dac_data_s[9]} {i_system_wrapper/system_i/axi_ad9361/inst/dac_data_s[10]} {i_system_wrapper/system_i/axi_ad9361/inst/dac_data_s[11]} {i_system_wrapper/system_i/axi_ad9361/inst/dac_data_s[12]} {i_system_wrapper/system_i/axi_ad9361/inst/dac_data_s[13]} {i_system_wrapper/system_i/axi_ad9361/inst/dac_data_s[14]} {i_system_wrapper/system_i/axi_ad9361/inst/dac_data_s[15]} {i_system_wrapper/system_i/axi_ad9361/inst/dac_data_s[16]} {i_system_wrapper/system_i/axi_ad9361/inst/dac_data_s[17]} {i_system_wrapper/system_i/axi_ad9361/inst/dac_data_s[18]} {i_system_wrapper/system_i/axi_ad9361/inst/dac_data_s[19]} {i_system_wrapper/system_i/axi_ad9361/inst/dac_data_s[20]} {i_system_wrapper/system_i/axi_ad9361/inst/dac_data_s[21]} {i_system_wrapper/system_i/axi_ad9361/inst/dac_data_s[22]} {i_system_wrapper/system_i/axi_ad9361/inst/dac_data_s[23]} ]]
+create_debug_port u_ila_0 probe
+set_property port_width 1 [get_debug_ports u_ila_0/probe7]
+set_property PROBE_TYPE DATA_AND_TRIGGER [get_debug_ports u_ila_0/probe7]
+connect_debug_port u_ila_0/probe7 [get_nets [list i_system_wrapper/system_i/ecm_top/U0/w_Adc_valid ]]
+create_debug_port u_ila_0 probe
+set_property port_width 1 [get_debug_ports u_ila_0/probe8]
+set_property PROBE_TYPE DATA_AND_TRIGGER [get_debug_ports u_ila_0/probe8]
+connect_debug_port u_ila_0/probe8 [get_nets [list i_system_wrapper/system_i/axi_ad9361/inst/dac_valid_i0_s ]]
+create_debug_port u_ila_0 probe
+set_property port_width 1 [get_debug_ports u_ila_0/probe9]
+set_property PROBE_TYPE DATA_AND_TRIGGER [get_debug_ports u_ila_0/probe9]
+connect_debug_port u_ila_0/probe9 [get_nets [list i_system_wrapper/system_i/axi_ad9361/inst/dac_valid_i1_s ]]
+create_debug_port u_ila_0 probe
+set_property port_width 1 [get_debug_ports u_ila_0/probe10]
+set_property PROBE_TYPE DATA_AND_TRIGGER [get_debug_ports u_ila_0/probe10]
+connect_debug_port u_ila_0/probe10 [get_nets [list i_system_wrapper/system_i/axi_ad9361/inst/i_tdd/tdd_rx_rf_en_reg ]]
+create_debug_port u_ila_0 probe
+set_property port_width 1 [get_debug_ports u_ila_0/probe11]
+set_property PROBE_TYPE DATA_AND_TRIGGER [get_debug_ports u_ila_0/probe11]
+connect_debug_port u_ila_0/probe11 [get_nets [list i_system_wrapper/system_i/axi_ad9361/inst/i_tdd/tdd_tx_vco_en_reg ]]
 
 set_property target_constrs_file system_constr.xdc [current_fileset -constrset]
 save_constraints -force
